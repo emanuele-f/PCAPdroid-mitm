emanuele-f/PCAPdroid-mitm

commit 22b80fa654ed602720748d6153770dc041cb1176

Add support for transparent proxy mode

app/build.gradle
@@ -10,8 +10,8 @@ android {
         applicationId "com.pcapdroid.mitm"
         minSdk 21
         targetSdk 32
-        versionCode 7
-        versionName "0.7"
+        versionCode 8
+        versionName "0.8"
 
         // see productFlavors below
         /*ndk {

app/src/main/python/mitm.py
@@ -61,7 +61,7 @@ running = False
 
 # Entrypoint: runs mitmproxy
 # From mitmproxy.tools.main.run, without the signal handlers
-def run(fd: int, dump_keylog: bool, mitm_args: str):
+def run(fd: int, dump_client: bool, dump_keylog: bool, mitm_args: str):
     global master
     global running
     running = True
@@ -78,7 +78,7 @@ def run(fd: int, dump_keylog: bool, mitm_args: str):
                 process_options(parser, opts, args)
                 checkCertificate()
 
-                pcapdroid = PCAPdroid(sock, dump_keylog)
+                pcapdroid = PCAPdroid(sock, dump_client, dump_keylog)
                 master.addons.add(pcapdroid)
 
                 ConnectionHandler.server_event = lambda handler, ev: server_event_proxy(pcapdroid, handler, ev)

app/src/main/python/pcapdroid.py
@@ -54,19 +54,26 @@ class MsgType(Enum):
     MASTER_SECRET = "secret"
 
 class PCAPdroid:
-    def __init__(self, sock: socket.socket, dump_master_secrets: bool):
+    def __init__(self, sock: socket.socket, dump_client: bool, dump_master_secrets: bool):
         self.sock = sock
+        self.dump_client = dump_client
 
         if dump_master_secrets:
             mitmproxy.net.tls.log_master_secret = self.log_master_secret
         else:
             mitmproxy.net.tls.log_master_secret = None
 
-    def send_message(self, tstamp: float, client_conn: mitmproxy.connection.Client, payload_type: MsgType, payload):
-        client_port = client_conn.peername[1] if client_conn else 0
+    def send_message(self, tstamp: float, client_conn: mitmproxy.connection.Client,
+            server_conn: mitmproxy.connection.Server, payload_type: MsgType, payload):
+        port = 0
+        if self.dump_client and client_conn:
+            port = client_conn.peername[1]
+        elif not self.dump_client and server_conn:
+            port = server_conn.sockname[1]
+
         tstamp_millis = int((tstamp or time.time()) * 1000)
 
-        header = "%u:%u:%s:%u\n" % (tstamp_millis, client_port, payload_type.value, len(payload))
+        header = "%u:%u:%s:%u\n" % (tstamp_millis, port, payload_type.value, len(payload))
         #ctx.log.debug(header)
 
         try:
@@ -81,18 +88,18 @@ class PCAPdroid:
                 ctx.master.shutdown()
 
     def running(self):
-        self.send_message(time.time(), None, MsgType.RUNNING, b'')
+        self.send_message(time.time(), None, None, MsgType.RUNNING, b'')
 
     def server_error(self, data: server_hooks.ServerConnectionHookData):
-        self.send_message(time.time(), data.client, MsgType.TCP_ERROR, data.server.error.encode("ascii"))
+        self.send_message(time.time(), data.client, data.server, MsgType.TCP_ERROR, data.server.error.encode("ascii"))
 
     def request(self, flow: http.HTTPFlow):
         if flow.request:
-            self.send_message(flow.request.timestamp_start, flow.client_conn, MsgType.HTTP_REQUEST, assemble_request(flow.request))
+            self.send_message(flow.request.timestamp_start, flow.client_conn, flow.server_conn, MsgType.HTTP_REQUEST, assemble_request(flow.request))
 
     def response(self, flow: http.HTTPFlow) -> None:
         if flow.response:
-            self.send_message(flow.response.timestamp_start, flow.client_conn, MsgType.HTTP_REPLY, assemble_response(flow.response))
+            self.send_message(flow.response.timestamp_start, flow.client_conn, flow.server_conn, MsgType.HTTP_REPLY, assemble_response(flow.response))
 
     def tcp_message(self, flow: mitmproxy.tcp.TCPFlow):
         msg = flow.messages[-1]
@@ -100,7 +107,7 @@ class PCAPdroid:
              return
 
         payload_type = MsgType.TCP_CLIENT_MSG if msg.from_client else MsgType.TCP_SERVER_MSG
-        self.send_message(msg.timestamp, flow.client_conn, payload_type, msg.content)
+        self.send_message(msg.timestamp, flow.client_conn, flow.server_conn, payload_type, msg.content)
 
     def websocket_message(self, flow: http.HTTPFlow):
         msg = flow.websocket.messages[-1]
@@ -108,22 +115,22 @@ class PCAPdroid:
             return
 
         payload_type = MsgType.WEBSOCKET_CLIENT_MSG if msg.from_client else MsgType.WEBSOCKET_SERVER_MSG
-        self.send_message(msg.timestamp, flow.client_conn, payload_type, msg.content)
+        self.send_message(msg.timestamp, flow.client_conn, flow.server_conn, payload_type, msg.content)
 
     def log_master_secret(self, ssl_connection, keymaterial: bytes):
-        self.send_message(time.time(), None, MsgType.MASTER_SECRET, keymaterial)
+        self.send_message(time.time(), None, None, MsgType.MASTER_SECRET, keymaterial)
 
     def tls_failed_client(self, data: mitmproxy.tls.TlsData):
-        self.send_message(time.time(), data.context.client, MsgType.TLS_ERROR, data.conn.error.encode("ascii"))
+        self.send_message(time.time(), data.context.client, data.context.server, MsgType.TLS_ERROR, data.conn.error.encode("ascii"))
 
     def tls_failed_server(self, data: mitmproxy.tls.TlsData):
-        self.send_message(time.time(), data.context.client, MsgType.TLS_ERROR, data.conn.error.encode("ascii"))
+        self.send_message(time.time(), data.context.client, data.context.server, MsgType.TLS_ERROR, data.conn.error.encode("ascii"))
 
     def error(self, flow: http.HTTPFlow):
-        self.send_message(time.time(), flow.context.client, MsgType.HTTP_ERROR, flow.error.encode("ascii"))
+        self.send_message(time.time(), flow.context.client, data.context.server, MsgType.HTTP_ERROR, flow.error.encode("ascii"))
 
     def tcp_error(self, flow: mitmproxy.tcp.TCPFlow):
-        self.send_message(time.time(), flow.context.client, MsgType.TCP_ERROR, flow.error.encode("ascii"))
+        self.send_message(time.time(), flow.context.client, data.context.server, MsgType.TCP_ERROR, flow.error.encode("ascii"))
 
     def add_log(self, entry: mitmproxy.log.LogEntry):
         lvl = str2lvl.get(entry.level, Log.DEBUG)

app/src/main/java/com/pcapdroid/mitm/MitmAPI.java
@@ -37,6 +37,7 @@ public class MitmAPI {
 
     public static final class MitmConfig implements Serializable {
         public int proxyPort;              // the SOCKS5 port to use to accept mitm-ed connections
+        public boolean transparentMode;    // true to use transparent proxy mode, false to use SOCKS5 proxy mode
         public boolean sslInsecure;        // true to disable upstream certificate check
         public boolean dumpMasterSecrets;  // true to enable the TLS master secrets dump messages (similar to SSLKEYLOG)
         public String proxyAuth;           // SOCKS5 proxy authentication, "user:pass"

app/src/main/java/com/pcapdroid/mitm/MitmService.java
@@ -120,12 +120,18 @@ public class MitmService extends Service implements Runnable {
     private String getMitmproxyArgs() {
         StringBuilder builder = new StringBuilder();
 
-        builder.append("-q --set onboarding=false --mode socks5 --listen-host 127.0.0.1 -p ");
+        builder.append("-q --set onboarding=false --listen-host 127.0.0.1 -p ");
         builder.append(mConf.proxyPort);
 
-        if(mConf.proxyAuth != null) {
-            builder.append(" --proxyauth ");
-            builder.append(mConf.proxyAuth);
+        if(mConf.transparentMode) {
+            builder.append(" --mode transparent");
+        } else {
+            builder.append(" --mode socks5");
+
+            if(mConf.proxyAuth != null) {
+                builder.append(" --proxyauth ");
+                builder.append(mConf.proxyAuth);
+            }
         }
 
         if(mConf.sslInsecure)
@@ -139,8 +145,12 @@ public class MitmService extends Service implements Runnable {
         String args = getMitmproxyArgs();
         Log.d(TAG, "mitmdump " + args);
 
+        // SOCKS5 mode is used with VPNService, so we must dump the client (original) connection
+        // Transparent mode is used with root mode where we capture the internet interface, so we must dump the server connection
+        boolean dump_client = !mConf.transparentMode;
+
         try {
-            mitm.callAttr("run", mFd.getFd(), mConf.dumpMasterSecrets, args);
+            mitm.callAttr("run", mFd.getFd(), dump_client, mConf.dumpMasterSecrets, args);
         } finally {
             try {
                 if(mFd != null)

commit e74396421c0b0acb1a590831a90c7c03b83dd7b3

mitmproxy 9.0.1 API fixes
This also fixes the following error with HTTP flows introduced in 22b80fa6:
AttributeError: 'HTTPFlow' object has no attribute 'context'

app/build.gradle
@@ -18,8 +18,8 @@ android {
         applicationId "com.pcapdroid.mitm"
         minSdk 21
         targetSdk 33
-        versionCode 12
-        versionName "0.12"
+        versionCode 13
+        versionName "0.13"
 
         // see productFlavors below
         /*ndk {

app/src/main/python/callback_logger.py
@@ -1,0 +1,23 @@
+import asyncio
+import logging
+from collections.abc import Callable
+from mitmproxy import log
+from mitmproxy.log import LogEntry
+
+# from mitmproxy eventstore.py
+class CallbackLogger(log.MitmLogHandler):
+    def __init__(
+            self,
+            callback: Callable[[LogEntry], None],
+    ):
+        super().__init__()
+        self.callback = callback
+        self.event_loop = asyncio.get_running_loop()
+        self.formatter = log.MitmFormatter(colorize=False)
+
+    def emit(self, record: logging.LogRecord) -> None:
+        entry = LogEntry(
+            msg=self.format(record),
+            level=log.LOGGING_LEVELS_TO_LOGENTRY.get(record.levelno, "error"),
+        )
+        self.event_loop.call_soon_threadsafe(self.callback, entry)

app/src/main/python/mitm.py
@@ -32,6 +32,7 @@ from mitmproxy.proxy.events import OpenConnectionCompleted
 from pcapdroid import PCAPdroid, AddonOpts
 from pathlib import Path
 import mitmproxy
+import traceback
 import socket
 import asyncio
 import sys
@@ -55,6 +56,8 @@ class StdErr:
     def write(self, msg):
         if pcapdroid:
             pcapdroid.log_warn(msg)
+    def flush(self):
+        pass
 
 sys.stdout = StdOut()
 sys.stderr = StdErr()
@@ -113,12 +116,14 @@ def run(fd: int, dump_client: bool, dump_keylog: bool, short_payload: bool, mitm
                 # stopped to properly close the TCP socket.
                 proxyserver = master.addons.lookup.get("proxyserver")
                 if proxyserver:
+                    # see test_proxyserver.py
                     print("Stopping proxyserver...")
-                    await proxyserver.shutdown_server()
+                    master.options.update(server=False)
+                    await proxyserver.setup_servers()
 
             asyncio.run(main())
-    except Exception as e:
-        print(e)
+    except Exception:
+        print(traceback.format_exc())
 
     print("mitmdump stopped")
     master = None

app/src/main/python/pcapdroid.py
@@ -15,7 +15,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with PCAPdroid.  If not, see <http://www.gnu.org/licenses/>.
 #
-#  Copyright 2022 - Emanuele Faranda
+#  Copyright 2023 - Emanuele Faranda
 #
 
 import socket
@@ -26,8 +26,10 @@ import traceback
 from mitmproxy import http, ctx
 from mitmproxy.net.http.http1.assemble import assemble_request, assemble_response
 from mitmproxy.proxy import server_hooks
+from mitmproxy.log import LogEntry
 from enum import Enum
 from java import jclass
+from callback_logger import CallbackLogger
 
 Log = jclass("android.util.Log")
 
@@ -69,15 +71,27 @@ class FlowData:
     response_sent: bool = False
     truncated: bool = False
 
+# A mitmproxy addon
+# See https://docs.mitmproxy.org/stable/api/events.html
 class PCAPdroid:
     def __init__(self, sock: socket.socket, opts: AddonOpts):
         self.sock = sock
         self.opts = opts
+        self.shutting_down = False
+
+        # intercept log events from mitmproxy
+        self.logger = CallbackLogger(self._add_log)
+        self.logger.install()
 
         if opts.dump_keylog:
             mitmproxy.net.tls.log_master_secret = self.log_master_secret
         else:
             mitmproxy.net.tls.log_master_secret = None
+
+    # override
+    def done(self):
+        print("PCAPdroid done")
+        self.logger.uninstall()
 
     def send_message(self, tstamp: float, client_conn: mitmproxy.connection.Client,
             server_conn: mitmproxy.connection.Server, payload_type: MsgType, payload: bytes):
@@ -90,18 +104,21 @@ class PCAPdroid:
         tstamp_millis = int((tstamp or time.time()) * 1000)
 
         header = "%u:%u:%s:%u\n" % (tstamp_millis, port, payload_type.value, len(payload))
-        #ctx.log.debug(header)
 
         try:
             self.sock.sendall(header.encode('ascii'))
             self.sock.sendall(payload)
         except socket.error as e:
             if e.errno == errno.EPIPE:
-                ctx.log.info("PCAPdroid closed")
-                ctx.master.shutdown()
+                if not self.shutting_down:
+                    self.shutting_down = True
+                    print("PCAPdroid closed")
+                    ctx.master.shutdown()
             else:
-                ctx.log.error(e)
-                ctx.master.shutdown()
+                if not self.shutting_down:
+                    self.shutting_down = True
+                    print(e)
+                    ctx.master.shutdown()
 
     def getFlowData(self, flow):
         # Extend the flow with additional data
@@ -141,7 +158,6 @@ class PCAPdroid:
     def running(self):
         self.send_message(time.time(), None, None, MsgType.RUNNING, b'')
 
-    # override
     def server_error(self, data: server_hooks.ServerConnectionHookData):
         self.send_message(time.time(), data.client, data.server, MsgType.TCP_ERROR, data.server.error.encode("ascii"))
 
@@ -210,11 +226,11 @@ class PCAPdroid:
 
     # override
     def error(self, flow: http.HTTPFlow):
-        self.send_message(time.time(), flow.context.client, data.context.server, MsgType.HTTP_ERROR, flow.error.encode("ascii"))
+        self.send_message(time.time(), flow.client_conn, flow.server_conn, MsgType.HTTP_ERROR, flow.error.msg.encode("ascii"))
 
     # override
     def tcp_error(self, flow: mitmproxy.tcp.TCPFlow):
-        self.send_message(time.time(), flow.context.client, data.context.server, MsgType.TCP_ERROR, flow.error.encode("ascii"))
+        self.send_message(time.time(), flow.context.client, flow.context.server, MsgType.TCP_ERROR, flow.error.msg.encode("ascii"))
 
     def log(self, msg, lvl=Log.INFO):
         Log.println(lvl, "mitmproxy", msg)
@@ -227,8 +243,7 @@ class PCAPdroid:
     def log_warn(self, msg):
         self.log(msg, lvl=Log.WARN)
 
-    # override
-    def add_log(self, entry: mitmproxy.log.LogEntry):
+    def _add_log(self, entry: LogEntry):
         lvl = str2lvl.get(entry.level, Log.DEBUG)
 
         if lvl >= Log.ERROR: